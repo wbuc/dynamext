673f2181b6f6a28b39abb8c84d623dd13ddfa447
Wessel Buchling <wbuchling@live.com>
2020-03-20 22:45:36 +0000

    dialog implementation started

--- src/modules/admin/components/Accounts.Dashboard.vue
+++ src/modules/admin/components/Accounts.Dashboard.vue
@@ -18,6 +18,21 @@
                     ></x-data-table>
                </v-col>
           </v-row>
+          <v-dialog v-model="showDialog" max-width="290">
+               <v-card>
+                    <v-card-title class="headline">Use Google's location service?</v-card-title>
+
+                    <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+
+                    <v-card-actions>
+                         <v-spacer></v-spacer>
+
+                         <v-btn color="green darken-1" text @click="showDialog = false">Disagree</v-btn>
+
+                         <v-btn color="green darken-1" text @click="showDialog = false">Agree</v-btn>
+                    </v-card-actions>
+               </v-card>
+          </v-dialog>
      </v-container>
 </template>
 
@@ -60,6 +75,7 @@ export default {
                               icon: "bar_chart",
                               color: "pink accent-3",
                               action: items => {
+                                   this.showDialog = true;
                                    // List of selected items in the data table.
                                    console.log(items);
                               }
@@ -97,7 +113,8 @@ export default {
                          action: item => console.log("deleting ", item.email)
                     }
                ],
-               quickEditFields: [{ name: "email" }]
+               quickEditFields: [{ name: "email" }],
+               showDialog: false
           };
      },
      methods: {
