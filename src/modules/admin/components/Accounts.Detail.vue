26094cc25a54842e2637c8fd30f582d86b4e9306
Wessel Buchling <wbuchling@live.com>
2020-03-24 21:56:47 +0000

    New item dialog updated for full page.

--- src/modules/admin/components/Accounts.Detail.vue
+++ src/modules/admin/components/Accounts.Detail.vue
@@ -1,28 +1,78 @@
 <template>
-     <v-dialog v-model="showDialog" max-width="290">
+     <v-dialog v-model="showDetail" fullscreen hide-overlay transition="dialog-bottom-transition">
           <v-card>
-               <v-card-title class="headline">Use Google's location service?</v-card-title>
-
-               <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
-
-               <v-card-actions>
+               <v-toolbar dark color="primary">
+                    <v-btn icon dark @click="closePage">
+                         <v-icon>mdi-close</v-icon>
+                    </v-btn>
+                    <v-toolbar-title>Settings</v-toolbar-title>
                     <v-spacer></v-spacer>
-
-                    <v-btn color="green darken-1" text @click="closePage">Close</v-btn>
-
-                    <v-btn color="green darken-1" text @click="savePage">Save</v-btn>
-               </v-card-actions>
+                    <v-toolbar-items>
+                         <v-btn dark text @click="closePage">Save</v-btn>
+                    </v-toolbar-items>
+               </v-toolbar>
+               <v-list three-line subheader>
+                    <v-subheader>User Controls</v-subheader>
+                    <v-list-item>
+                         <v-list-item-content>
+                              <v-list-item-title>Content filtering</v-list-item-title>
+                              <v-list-item-subtitle>Set the content filtering level to restrict apps that can be downloaded</v-list-item-subtitle>
+                         </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                         <v-list-item-content>
+                              <v-list-item-title>Password</v-list-item-title>
+                              <v-list-item-subtitle>Require password for purchase or use password to restrict purchase</v-list-item-subtitle>
+                         </v-list-item-content>
+                    </v-list-item>
+               </v-list>
+               <v-divider></v-divider>
+               <v-list three-line subheader>
+                    <v-subheader>General</v-subheader>
+                    <v-list-item>
+                         <v-list-item-action>
+                              <v-checkbox v-model="data.notifications"></v-checkbox>
+                         </v-list-item-action>
+                         <v-list-item-content>
+                              <v-list-item-title>Notifications</v-list-item-title>
+                              <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
+                         </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                         <v-list-item-action>
+                              <v-checkbox v-model="data.sound"></v-checkbox>
+                         </v-list-item-action>
+                         <v-list-item-content>
+                              <v-list-item-title>Sound</v-list-item-title>
+                              <v-list-item-subtitle>Auto-update apps at any time. Data charges may apply</v-list-item-subtitle>
+                         </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                         <v-list-item-action>
+                              <v-checkbox v-model="data.widgets"></v-checkbox>
+                         </v-list-item-action>
+                         <v-list-item-content>
+                              <v-list-item-title>Auto-add widgets</v-list-item-title>
+                              <v-list-item-subtitle>Automatically add home screen widgets</v-list-item-subtitle>
+                         </v-list-item-content>
+                    </v-list-item>
+               </v-list>
           </v-card>
      </v-dialog>
 </template>
 
 <script>
 export default {
-     name: "AccountsDetail",
+     name: "Accounts.Detail",
      props: ["showDialog"],
      data() {
           return {
-               showMe: false
+               showMe: false,
+               data: {
+                    notifications: false,
+                    sound: true,
+                    widgets: false
+               }
           };
      },
      computed: {
