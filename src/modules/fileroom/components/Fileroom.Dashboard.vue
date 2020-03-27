517867ad5ec4413829811b78c833af0ad65e1287
Wessel Buchling <wbuchling@live.com>
2020-03-27 22:46:01 +0000

    Treeview now working with sample data and rough styling.

--- src/modules/fileroom/components/Fileroom.Dashboard.vue
+++ src/modules/fileroom/components/Fileroom.Dashboard.vue
@@ -2,14 +2,50 @@
      <v-row no-gutters>
           <v-col cols="12" md="3">
                <v-card tile flat>
-                    <v-tabs v-model="tab" fixed-tabs background-color dark>
-                         <v-tab v-for="item in items" :key="item.tab">{{ item.tab }}</v-tab>
+                    <v-tabs v-model="activeTab" fixed-tabs background-color dark>
+                         <v-tabs-slider color="red"></v-tabs-slider>
+                         <v-tab key="Fileroom">Fileroom</v-tab>
+                         <v-tab key="Personal">
+                              <v-icon class="mr-2" small color="yellow darken-3">mdi-star-outline</v-icon>Favourites
+                         </v-tab>
                     </v-tabs>
 
-                    <v-tabs-items v-model="tab">
-                         <v-tab-item v-for="item in items" :key="item.tab">
+                    <v-tabs-items v-model="activeTab">
+                         <v-tab-item key="Fileroom">
                               <v-card flat>
-                                   <v-card-text>{{ item.component }}</v-card-text>
+                                   <v-treeview
+                                        v-model="tree"
+                                        :open="open"
+                                        :items="items"
+                                        item-key="name"
+                                        open-on-click="true"
+                                        activatable
+                                        color="warning"
+                                        selectable="true"
+                                        selected-color="error"
+                                        transition="true"
+                                        dense
+                                   >
+                                        <template v-slot:prepend="{ item, open }">
+                                             <v-icon
+                                                  v-if="!item.file"
+                                                  color="info"
+                                             >{{ open ? 'mdi-folder-open' : 'mdi-folder' }}</v-icon>
+                                             <v-icon v-else>{{ files[item.file] }}</v-icon>
+                                        </template>
+
+                                        <template v-slot:label="{ item }">
+                                             <b>{{item.name}}</b>
+                                             <v-btn icon v-on:click.stop="editme(item.name)">
+                                                  <v-icon small>edit</v-icon>
+                                             </v-btn>
+                                        </template>
+                                   </v-treeview>
+                              </v-card>
+                         </v-tab-item>
+                         <v-tab-item key="Personal">
+                              <v-card flat>
+                                   <v-card-text>And my place!</v-card-text>
                               </v-card>
                          </v-tab-item>
                     </v-tabs-items>
@@ -26,10 +62,104 @@ export default {
      name: "Fileroom.Dashboard",
      data() {
           return {
-               tab: null,
+               activeTab: null,
+               open: ["public"],
+               files: {
+                    html: "mdi-language-html5",
+                    js: "mdi-nodejs",
+                    json: "mdi-json",
+                    md: "mdi-markdown",
+                    pdf: "mdi-file-pdf",
+                    png: "mdi-file-image",
+                    txt: "mdi-file-document-outline",
+                    doc: "mdi-file",
+                    xls: "mdi-file-excel"
+               },
+               tree: [],
                items: [
-                    { tab: "Primary", component: "Tab 1 Content" },
-                    { tab: "Personal", component: "Tab 2 Content" }
+                    {
+                         name: "1 Corporate",
+                         children: [
+                              {
+                                   name: "1.2 Board Minutes",
+                                   children: [
+                                        { name: "1.2.1 2017 Board Minutes" },
+                                        { name: "1.2.2 2018 Board Minutes" }
+                                   ]
+                              },
+                              {
+                                   name: "1.3 Cert of Incorporation",
+                                   children: [
+                                        {
+                                             name:
+                                                  " 1.3.1 AUS_Certificate of Registration",
+                                             file: "doc"
+                                        },
+                                        {
+                                             name:
+                                                  "1.3.2 BR_Gründungsdokument_Sample Brasil",
+                                             file: "doc"
+                                        },
+                                        { name: "1.3.3 CN_FB", file: "doc" },
+                                        { name: "1.3.4 FR_FB", file: "doc" },
+                                        { name: "1.3.5 TestG_DE", file: "doc" }
+                                   ]
+                              },
+                              { name: "1.5 Intercompany" },
+                              { name: "1.6 Org Charts" },
+                              { name: "1.7 Prior Names" },
+                              { name: "1.8 Articles of Incorporation" }
+                         ]
+                    },
+                    {
+                         name: "node_modules"
+                    },
+                    {
+                         name: "public",
+                         children: [
+                              {
+                                   name: "static",
+                                   children: [
+                                        {
+                                             name: "logo.png",
+                                             file: "png"
+                                        }
+                                   ]
+                              },
+                              {
+                                   name: "favicon.ico",
+                                   file: "png"
+                              },
+                              {
+                                   name: "index.html",
+                                   file: "html"
+                              }
+                         ]
+                    },
+                    {
+                         name: ".gitignore",
+                         file: "txt"
+                    },
+                    {
+                         name: "babel.config.js",
+                         file: "js"
+                    },
+                    {
+                         name: "package.json",
+                         file: "json"
+                    },
+                    {
+                         name: "README.md",
+                         file: "md"
+                    },
+                    {
+                         name: "vue.config.js",
+                         file: "js"
+                    },
+                    {
+                         name: "yarn.lock",
+                         file: "txt"
+                    }
                ]
           };
      }
