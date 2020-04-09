bcf02c9e8743eb066facbbfae794a4e4595a135a
Wessel Buchling <wbuchling@live.com>
2020-04-09 19:41:43 +0100

    Started with the draggable controls in Schedule builder

--- src/modules/schedules/components/Schedules.Dashboard.vue
+++ src/modules/schedules/components/Schedules.Dashboard.vue
@@ -1,6 +1,45 @@
 <template>
      <v-row>
-          <v-col cols="12" :md="explorerConfig.fullView ? '7':'4'">Left</v-col>
+          <v-col cols="12" :md="explorerConfig.fullView ? '7':'4'">
+               <div class="row">
+                    <div class="col-3">
+                         <h3>Draggable 1</h3>
+                         <draggable
+                              class="dragArea list-group"
+                              :list="list1"
+                              :group="{ name: 'people', pull: 'clone', put: false }"
+                              :clone="cloneControl"
+                              @change="log"
+                         >
+                              <div
+                                   class="list-group-item"
+                                   v-for="element in list1"
+                                   :key="element.id"
+                              >{{ element.name }}</div>
+                         </draggable>
+                    </div>
+
+                    <div class="col-3">
+                         <h3>Draggable 2</h3>
+                         <draggable
+                              class="dragArea list-group"
+                              :list="list2"
+                              group="people"
+                              @change="log"
+                         >
+                              <div
+                                   class="list-group-item"
+                                   v-for="element in list2"
+                                   :key="element.id"
+                              >{{ element.name }}</div>
+                         </draggable>
+                    </div>
+
+                    <rawDisplayer class="col-3" :value="list1" title="List 1" />
+
+                    <rawDisplayer class="col-3" :value="list2" title="List 2" />
+               </div>
+          </v-col>
           <v-col cols="12" :md="explorerConfig.fullView ? '5':'8'">Right</v-col>
      </v-row>
 </template>
@@ -8,9 +47,12 @@
 <script>
 import { mapGetters } from "vuex";
 
+import draggable from "vuedraggable";
+
+let idGlobal = 9;
 export default {
      name: "Schedules.Dashboard",
-     components: {},
+     components: { draggable },
      computed: {
           ...mapGetters(["api"])
      },
@@ -18,10 +60,32 @@ export default {
           return {
                explorerConfig: {
                     fullView: true
-               }
+               },
+               list1: [
+                    { name: "John", id: 1 },
+                    { name: "Joao", id: 2 },
+                    { name: "Jean", id: 3 },
+                    { name: "Gerard", id: 4 }
+               ],
+               list2: [
+                    { name: "Juan", id: 5 },
+                    { name: "Edgard", id: 6 },
+                    { name: "Johnson", id: 7 }
+               ]
           };
      },
-     methods: {},
+     methods: {
+          log: function(evt) {
+               window.console.log(evt);
+          },
+          cloneControl(item) {
+               console.log(item);
+               return {
+                    id: idGlobal++,
+                    name: `Control ${item.name}`
+               };
+          }
+     },
      created() {}
 };
 </script>
