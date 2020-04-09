e2d7c108ca4743007466f2fc68f72b85d800516f
Wessel Buchling <wbuchling@live.com>
2020-04-09 22:56:56 +0100

    Schedule builder started

--- src/modules/schedules/components/Schedules.Dashboard.vue
+++ src/modules/schedules/components/Schedules.Dashboard.vue
@@ -1,46 +1,71 @@
 <template>
      <v-row>
           <v-col cols="12" :md="explorerConfig.fullView ? '7':'4'">
-               <div class="row">
-                    <div class="col-3">
-                         <h3>Draggable 1</h3>
+               <v-card class="x-toolbox">
+                    <v-list width="100%">
+                         <v-subheader>Toolbox</v-subheader>
+
                          <draggable
                               class="dragArea list-group"
                               :list="list1"
-                              :group="{ name: 'people', pull: 'clone', put: false }"
+                              :group="{ name: 'toolbox', pull: 'clone', put: false }"
                               :clone="cloneControl"
                               @change="log"
                          >
-                              <div
-                                   class="list-group-item"
+                              <v-list-item
+                                   two-line
                                    v-for="element in list1"
                                    :key="element.id"
-                              >{{ element.name }}</div>
+                                   color="primary"
+                                   class="x-control"
+                              >
+                                   <v-list-item-action>
+                                        <v-icon>{{element.icon}}</v-icon>
+                                   </v-list-item-action>
+                                   <v-list-item-content>
+                                        <v-list-item-title>{{ element.name }}</v-list-item-title>
+                                        <v-list-item-subtitle>This is some control information to help you.</v-list-item-subtitle>
+                                   </v-list-item-content>
+                              </v-list-item>
                          </draggable>
-                    </div>
+                    </v-list>
+               </v-card>
+               <rawDisplayer class="col-3" :value="list1" title="List 1" />
 
-                    <div class="col-3">
-                         <h3>Draggable 2</h3>
+               <rawDisplayer class="col-3" :value="list2" title="List 2" />
+          </v-col>
+          <v-col cols="12" :md="explorerConfig.fullView ? '5':'8'">
+               <v-card class="x-form">
+                    <v-list width="100%">
+                         <v-subheader>New Project Schedule</v-subheader>
                          <draggable
                               class="dragArea list-group"
                               :list="list2"
-                              group="people"
+                              group="toolbox"
                               @change="log"
+                              handle=".x-control-handle"
                          >
-                              <div
-                                   class="list-group-item"
+                              <v-list-item
+                                   two-line
                                    v-for="element in list2"
                                    :key="element.id"
-                              >{{ element.name }}</div>
+                                   color="primary"
+                                   class="x-control"
+                              >
+                                   <v-list-item-action class="x-control-handle">
+                                        <v-icon class="grey--text text--darken-1">mdi-drag</v-icon>
+                                   </v-list-item-action>
+                                   <v-list-item-action>
+                                        <v-icon>{{element.icon}}</v-icon>
+                                   </v-list-item-action>
+                                   <v-list-item-content>
+                                        <v-list-item-title>{{ element.name }}</v-list-item-title>
+                                   </v-list-item-content>
+                              </v-list-item>
                          </draggable>
-                    </div>
-
-                    <rawDisplayer class="col-3" :value="list1" title="List 1" />
-
-                    <rawDisplayer class="col-3" :value="list2" title="List 2" />
-               </div>
+                    </v-list>
+               </v-card>
           </v-col>
-          <v-col cols="12" :md="explorerConfig.fullView ? '5':'8'">Right</v-col>
      </v-row>
 </template>
 
@@ -59,18 +84,44 @@ export default {
      data() {
           return {
                explorerConfig: {
-                    fullView: true
+                    fullView: false
                },
                list1: [
-                    { name: "John", id: 1 },
-                    { name: "Joao", id: 2 },
-                    { name: "Jean", id: 3 },
-                    { name: "Gerard", id: 4 }
+                    {
+                         id: 1,
+                         name: "Text",
+                         description: "",
+                         icon: "mdi-format-text"
+                    },
+                    {
+                         id: 2,
+                         name: "Paragraph",
+                         description: "",
+                         icon: "mdi-format-pilcrow"
+                    },
+                    {
+                         id: 3,
+                         name: "Header",
+                         description: "",
+                         icon: "mdi-format-header-1"
+                    },
+                    {
+                         id: 4,
+                         name: "Decimal",
+                         description: "",
+                         icon: "mdi-decimal"
+                    },
+                    {
+                         id: 5,
+                         name: "Number",
+                         description: "",
+                         icon: "mdi-numeric-10"
+                    }
                ],
                list2: [
-                    { name: "Juan", id: 5 },
-                    { name: "Edgard", id: 6 },
-                    { name: "Johnson", id: 7 }
+                    { id: 91, name: "Header", icon: "mdi-format-header-1" },
+                    { id: 92, name: "Text", icon: "mdi-format-text" },
+                    { id: 93, name: "Paragraph", icon: "mdi-format-pilcrow" }
                ]
           };
      },
@@ -80,9 +131,11 @@ export default {
           },
           cloneControl(item) {
                console.log(item);
+               /* item.name and item.id */
                return {
                     id: idGlobal++,
-                    name: `Control ${item.name}`
+                    name: `Control ${item.name}`,
+                    icon: item.icon
                };
           }
      },
@@ -98,5 +151,15 @@ export default {
      position: -webkit-sticky;
      position: sticky;
      top: 100px;
+}
+.x-toolbox .x-control:hover {
+     cursor: move;
+     /* background-color: #f6f6f6; */
+}
+.x-form .x-control {
+     /* border: 1px solid #0000001f; */
+}
+.x-form .x-control-handle {
+     cursor: move;
 }
 </style>
