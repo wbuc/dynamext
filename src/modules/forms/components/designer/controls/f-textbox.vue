bbcb0f27a047bafdd125c866f8d79eac9db8f9b3
Wessel Buchling <wbuchling@live.com>
2020-06-06 11:57:44 +0100

    temp functional component added. need to check later why this is not working. But looks like vuetify is already using functional for most of it's components

---
+++ src/modules/forms/components/designer/controls/f-textbox.vue
@@ -0,0 +1,33 @@
+<template functional>
+     <div>
+          <v-list-item-title class="title font-weight-light">{{props.control.name}}</v-list-item-title>
+          <v-list-item-subtitle class="caption text--secondary">{{props.control.instruction}}</v-list-item-subtitle>
+          <v-text-field
+               style="width:100%"
+               outlined
+               single-line
+               hide-details
+               dense
+               disabled
+               v-model="props.control.value"
+               :placeholder="props.control.properties.placeholder"
+               @click.stop
+          ></v-text-field>
+     </div>
+     <!-- </div> -->
+</template>
+
+<script>
+export default {
+     name: "f.control.textbox.designer",
+     props: {
+          control: {
+               Type: Object,
+               default: () => ({})
+          }
+     }
+};
+</script>
+
+<style>
+</style>
